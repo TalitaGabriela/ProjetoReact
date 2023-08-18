5711eb0c08b0189a439ad00ee96513b264a3fb4e
Talita Gabriela Del Poso Vilalva <talitagabriela1216@gmail.com>
2023-08-18 14:11:39 -0400

    Commit

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -3,13 +3,16 @@ import { Link } from "react-router-dom";
 
 export default function Home(){
     const [count, setCount] = useState(0);
+    const [name, setName] = useState("");
     return(
         <div>
             <h1>Home</h1>
             <Link to="/todo">PARA FAZER</Link>
             <p>{count}</p>
+            <p>{name}</p>
             <button onClick={() => setCount(count+1)}>Count</button>
-            <button onClick={() => setCount(count*0)}>Reset</button>
+            <button onClick={() => setCount(0)}>Reset</button>
+            <input type="text" onChange={(e)=>{setName(e.target.value)}}></input>
         </div>
     );
 }
